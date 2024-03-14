ce0ec546e8d06b7e22d696d201f1eeb94d1dc806
yang.dani <yang.dani@northeastern.edu>
2024-03-14 12:30:32 -0400

    readUser and writeUser added

--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -2,3 +2,15 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const router = express.Router()
+
+router.post('/addUser', (req, res) => {
+    let newUser = req.body;
+    req.users.push(newUser);
+    fs.writeFile(path.resolve(__dirname, './data/users.json'), JSON.stringify(req.users), (err) => {
+        if (err) console.log('Failed to write');
+        else console.log('User Saved');
+      });
+      res.send('done');
+});
+
+module.exports = router
